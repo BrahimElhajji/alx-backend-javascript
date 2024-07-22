553d23a0250358b82aad8339885c9c38fe9bf4dd
BrahimElhajji <elhajji080898@gmail.com>
2024-07-22 15:15:15 -0700

    task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  /* If trueOrFalse is true, the variables inside this block will be separate */
+  // eslint-disable-line no-unused-vars
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
-  /* Return the outer task and task2, which are not affected by the inner block */
+  // eslint-disable-line no-unused-vars
 
   return [task, task2];
 }
