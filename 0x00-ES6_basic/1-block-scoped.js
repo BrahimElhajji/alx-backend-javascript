03cecbd129cb6a4325fa109c3a319bd745568cfe
BrahimElhajji <elhajji080898@gmail.com>
2024-07-22 15:08:23 -0700

    task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  // Declare task and task2 using let so they are block-scoped
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
-  // If trueOrFalse is true, the variables inside this block will be separate
+  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
-    // These task and task2 are block-scoped and will not overwrite the outer variables
-    let task = true;
-    let task2 = false;
+    const task = true;
+    const task2 = false;
   }
+  /* eslint-enable no-unused-vars */
 
-  // Return the outer task and task2, which are not affected by the inner block
   return [task, task2];
+}
