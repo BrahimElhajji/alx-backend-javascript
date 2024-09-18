534d572ec9f81f298f50c2ee0a44d68a8b8fe33b
BrahimElhajji <elhajji080898@gmail.com>
2024-09-18 14:51:24 +0000

    test

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,31 +3,22 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-
     const lines = data.split('\n').filter((line) => line.trim() !== '');
-
     if (lines.length <= 1) {
       throw new Error('Cannot load the database');
     }
-
     const students = lines.slice(1);
-
     const fieldData = {};
-
     for (const student of students) {
       const [firstname, , , field] = student.split(',');
-
       if (firstname && field) {
         if (!fieldData[field]) {
           fieldData[field] = [];
         }
-
         fieldData[field].push(firstname);
       }
     }
-
     console.log(`Number of students: ${students.length}`);
-
     for (const field in fieldData) {
       const list = fieldData[field].join(', ');
       console.log(`Number of students in ${field}: ${fieldData[field].length}. List: ${list}`);
