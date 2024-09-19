de5aefe5ff9182f01d01fc28bf1feb677749050d
BrahimElhajji <elhajji080898@gmail.com>
2024-09-19 14:09:28 +0000

    test

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,41 @@
+const fs = require('fs').promises;
+
+function countStudents(path) {
+	return fs.readFile(path, 'utf-8')
+		.then((data) => {
+			const lines = data.split('\n').filter(line => line.trim() !== '');
+
+			const header = lines.shift();
+
+			if (!header) {
+				throw new Error('Cannot load the database');
+			}
+
+			const students = {};
+			let totalStudents = 0;
+
+			for (const line of lines) {
+				const [firstname, lastname, age, field] = line.split(',');
+
+				if (firstname && field) {
+					totalStudents++;
+
+					if (!students[field]) {
+						students[field] = [];
+					}
+					students[field].push(firstname);
+				}
+			}
+
+			console.log(`Number of students: ${totalStudents}`);
+
+			for (const [field, names] of Object.entries(students)) {
+				console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+			}
+		})
+		.catch((err) => {
+			throw new Error('Cannot load the database');
+		});
+}
+
+module.exports = countStudents;
