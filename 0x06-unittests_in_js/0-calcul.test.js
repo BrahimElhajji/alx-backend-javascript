ffd8251801ff8c7ef8c4719dba7d5481b746663e
BrahimElhajji <elhajji080898@gmail.com>
2024-09-19 16:40:57 -0700

    test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -17,8 +17,4 @@ describe('calculateNumber', function() {
   it('should return 6 for inputs (1.5, 3.7)', function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
-
-  it('should return -2 for inputs (-1.4, -1.4)', function() {
-    assert.strictEqual(calculateNumber(-1.4, -1.4), -2);
-  });
 });
