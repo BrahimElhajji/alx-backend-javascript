6429936297640d2ef9c4a0ef40c7478a25d8af0f
BrahimElhajji <elhajji080898@gmail.com>
2024-09-19 20:15:35 -0700

    last test

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -43,20 +43,3 @@ describe('/login endpoint', () => {
       .expect('Welcome Betty', done);
   });
 });
-
-describe('Cart page', () => {
-  it('should return correct status code when :id is a number', (done) => {
-    request.get('http://localhost:7865/cart/12', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Payment methods for cart 12');
-      done();
-    });
-  });
-
-  it('should return 404 status code when :id is NOT a number', (done) => {
-    request.get('http://localhost:7865/cart/hello', (error, response, body) => {
-      expect(response.statusCode).to.equal(404);
-      done();
-    });
-  });
-});
