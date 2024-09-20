9c7df48d539746771e813564a24b53b77858fc1b
BrahimElhajji <elhajji080898@gmail.com>
2024-09-19 18:55:01 -0700

    done

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,12 @@
 const express = require('express');
+
 const app = express();
+const PORT = 7865;
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-if (require.main === module) {
-  app.listen(7865, () => {
-    console.log('API available on localhost port 7865');
-  });
-}
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,34 +1,16 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api'); // Import the Express app
-const http = require('http');
-
-let server;
-
-before(done => {
-  server = app.listen(7865, () => {
-    console.log('Server is running on port 7865');
-    done();
-  });
-});
-
-after(done => {
-  server.close(done);
-});
 
 describe('Index page', () => {
-  it('Returns status code 200', done => {
-    request('http://localhost:7865', (error, response, body) => {
-      if (error) return done(error);
+  it('should return correct status code', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
-  it('Returns the welcome message', done => {
-    request('http://localhost:7865', (error, response, body) => {
-      if (error) return done(error);
-      expect(response.statusCode).to.equal(200);
+  it('should return correct result', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
