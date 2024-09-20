3a24c9ed9a6c7c00a67006c099bd2b2367e69701
BrahimElhajji <elhajji080898@gmail.com>
2024-09-19 19:32:32 -0700

    test

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+const express = require('express');
+const app = require('./api'); // Make sure your api.js exports the app
+
+describe('Index page', () => {
+  it('should return the correct status code', (done) => {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+
+  it('should return the correct result', (done) => {
+    request(app)
+      .get('/')
+      .expect('Welcome to the payment system', done);
+  });
+});
+
+describe('/available_payments endpoint', () => {
+  it('should return available payment methods', (done) => {
+    request(app)
+      .get('/available_payments')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .expect((res) => {
+        expect(res.body).to.deep.equal({
+          payment_methods: {
+            credit_cards: true,
+            paypal: false
+          }
+        });
+      })
+      .end(done);
+  });
+});
+
+describe('/login endpoint', () => {
+  it('should return a welcome message', (done) => {
+    request(app)
+      .post('/login')
+      .send({ userName: 'Betty' })
+      .expect(200)
+      .expect('Welcome Betty', done);
+  });
+});
