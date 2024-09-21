e1d4823398c36a8e9685edf7dadf45d7622c77ee
BrahimElhajji <elhajji080898@gmail.com>
2024-09-20 19:12:07 -0700

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 // The server listens on port 1245
