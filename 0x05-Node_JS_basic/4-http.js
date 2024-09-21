b21261bb4b14d4d3345450d2789fb500d069f81c
BrahimElhajji <elhajji080898@gmail.com>
2024-09-20 19:09:42 -0700

    task 4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!\n');
+});
+
+// The server listens on port 1245
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+// Export the app for potential testing or further use
+module.exports = app;
